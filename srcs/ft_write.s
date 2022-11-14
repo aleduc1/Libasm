ff64975e12b75b54ca0df76725569d22b345aa8d
Antoine-dev <aleduc@student.42.fr>
2022-11-14 19:01:37 +0100

    Alignement issue, jump on syscall error is now clear, still have some trouble figuring out how return value can be negative number and how binary know when to operate with 2s complement or when to treat a binary as positive

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,3 +1,4 @@
+; Directives ;
 bits 64										; Uneeded since nasm -f elf64 enable it by default, just for me to know
 cpu x64										; Dont enable opcode instructions that arnt compatible with x64 arch
 default rel								; Enable RIP relative addressing by default, override with abs prefix
@@ -6,17 +7,19 @@ extern __errno_location		; function that evaluate to errno address, Cf errno.h
 global ft_write:function	; function is an ELF specific extension to the global directive
 %define SYS_write 0x01		; Preprocessor directive, Cf /usr/include/syscall.h
 
-section .text ;dont need to create a local stack frame because x64 + no local variables needed
+; Executable segment, opcode mmemonics ;
+section .text ; Dont need to create a local stack frame because x64 + no local variables needed
 ft_write: ; Like what the C implementation does, we write here a wrapper to the syscall write
-	mov rax, SYS_write ; in rax + other register already set if ABI compliant
+	mov rax, SYS_write ; In rax + other register already set if ABI compliant
 	syscall	; x86_64 native instruction instead of old interruption code
-	test rax, rax ; only check last bit because no negative in x64
-	js _error
+	test rax, rax ; bitwise AND on itself, test non zero and set SF if rax < 0
+	js _error ; Jump if SF, there is no negative addresses in x86/x64
 	ret ; Pop rip (return address left by caller via call instruction) and jump to it
 _error:
-	neg rax	; On linux systems -1 | -4095 are reverse errno value indicating an error on most syscall
+	neg rax	; Currently a negative errno, errno defines are only positive, neg does 2s complement
 	mov r10, rax ; Save errno value
-	call __errno_location WRT ..plt; WRT plt CF nasmdoc v2.15.05 chapter 9.2.5
+;	and rsp, 0xFFFF_FFFF_FFFF_FFF0 ; Align next 16 bytes bondary for 64 bit registers
+	call __errno_location WRT ..plt ; WRT plt CF nasmdoc v2.15.05 chapter 9.2.5
 	mov qword [rax], r10 ; Store errno value in errno memory
 	mov rax, -1	; Return value of our wrapper (man 2 write)
 	ret
