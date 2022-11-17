e11366d3d82981f31c93c48c9514e051ecaf8c0e
Antoine-dev <aleduc@student.42.fr>
2022-11-17 03:14:07 +0100

    Prototype & instructions recap done, took me a while to find prototypes and functions in old subjects, see C11 for list functions ref and ft_atoi_base in the school slack workspace, since it is no longer in subjects but my comment should be enough and up to date on it

--- includes/libasm_bonus.h
+++ includes/libasm_bonus.h
@@ -3,4 +3,46 @@
 
 #include <stdio.h>
 
+typedef struct	s_list
+{
+	void					*data;
+	struct s_list	*next;
+}								t_list;
+
+/* Return the number of element in the list */
+int	ft_list_size(t_list *begin_list);
+
+/*
+** Add a new elem of t_list at the beginning of the list
+** Assign data in this elem->data
+** Update head pointer if needed
+** ft_create_elem() allowed
+*/
+void	ft_list_push_front(t_list **begin_list, void *data);
+
+/*
+** Erase elem of the list whose data is equal to data is equal to the reference data
+** Function pointed to by cmp will be used as follow (*cmp)(list_ptr->data, data_ref)
+** cmp could be ft_strcmp
+** free() allowed
+*/
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
+
+/*
+** Sort the list content by ascending order by comparing two links 
+** thanks to a function that can compare the data held in those two links
+*/
+void	ft_list_sort(t_list **begin_list, int (*cmp)());
+
+/*
+** Behave like atoi except str is in a specific base passed are a second parameter
+** the atoi() function converts the initial portion of the string pointed to by nptr to int
+** If a parameter is incorrect the function return 0
+** 			An error can be:
+**			- Base is empty or 1
+**			- Base has two times the same character
+**			- Base contains +/- or whitespaces
+*/
+int		ft_atoi_base(char *str, char *base);
+
 #endif
